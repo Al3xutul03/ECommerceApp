16b7b20feb930a3c221adfd0b11b8b360c7774ad
Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
2025-06-28 18:57:59 +0300

    MySQLParser implementation

---
+++ ECommerceAppIntegrationTests/MySQLParserTests.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using Utils.MySQLInterface;
+
+namespace ECommerceApp.IntegrationTests
+{
+    [TestFixture]
+    public class MySQLParserTests
+    {
+        [Test]
+        public async Task GetTable_AdminTableWithNoFilter_FullTable()
+        {
+            // Arrange
+            string connectionFilePath = @"MySQL_connection.xml";
+            var connector = new MySQLConnector(connectionFilePath);
+            MySqlConnection connection = await connector.CreateConnection();
+
+            // Act
+            var parser = new MySQLParser(connection, SqlTable.AdminInfo);
+            DataTable dt = await parser.GetTable();
+
+            // Assert
+            if (dt.TableName != "admin_info") Assert.Fail();
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task GetTable_UserTableWithNoFilter_FullTable()
+        {
+            // Arrange
+            string connectionFilePath = @"MySQL_connection.xml";
+            var connector = new MySQLConnector(connectionFilePath);
+            MySqlConnection connection = await connector.CreateConnection();
+
+            // Act
+            var parser = new MySQLParser(connection, SqlTable.UserInfo);
+            DataTable dt = await parser.GetTable();
+
+            // Assert
+            if (dt.TableName != "user_info") Assert.Fail();
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task GetTable_AdminTableWithFilters_FullTable()
+        {
+            // Arrange
+            string connectionFilePath = @"MySQL_connection.xml";
+            var connector = new MySQLConnector(connectionFilePath);
+            MySqlConnection connection = await connector.CreateConnection();
+
+            // Act
+            var parser = new MySQLParser(connection, SqlTable.AdminInfo);
+            parser.Select(new List<SqlColumn> { SqlColumn.Username, SqlColumn.ID });
+            parser.SortBy(new List<SqlColumn> { SqlColumn.Username }, SortType.Descending);
+            DataTable dt = await parser.GetTable();
+
+            // Assert
+            if (dt.TableName != "admin_info") Assert.Fail();
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task GetTable_UserTableWithFilters_FullTable()
+        {
+            // Arrange
+            string connectionFilePath = @"MySQL_connection.xml";
+            var connector = new MySQLConnector(connectionFilePath);
+            MySqlConnection connection = await connector.CreateConnection();
+
+            // Act
+            var parser = new MySQLParser(connection, SqlTable.UserInfo);
+            parser.Select(new List<SqlColumn> { SqlColumn.Username });
+            parser.SortBy(new List<SqlColumn> { SqlColumn.ID }, SortType.Descending);
+            DataTable dt = await parser.GetTable();
+
+            // Assert
+            if (dt.TableName != "user_info") Assert.Fail();
+            Assert.Pass();
+        }
+    }
+}
---
+++ Utils/MySQLInterface/MySQLParser.cs
@@ -0,0 +1,100 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.MySQLInterface
+{
+    public class MySQLParser
+    {
+        private MySqlConnection connection;
+        
+        public SqlTable SqlTable { get { return sqlTable; } set { sqlTable = value; } }
+        private SqlTable sqlTable;
+
+        [AllowNull] private IEnumerable<SqlColumn> sortColumns;
+        [AllowNull] private IEnumerable<SqlColumn> selectColumns;
+        private SortType sortType;
+
+
+        private static Dictionary<SqlTable, string> tableStrings = new Dictionary<SqlTable, string>
+        {
+            { SqlTable.AdminInfo, "admin_info" },
+            { SqlTable.UserInfo, "user_info" }
+        };
+
+        private static Dictionary<SqlColumn, string> columnStrings = new Dictionary<SqlColumn, string>
+        {
+            { SqlColumn.ID, "id" },
+            { SqlColumn.Username, "username" },
+        };
+
+        private static Dictionary<SortType, string> sortStrings = new Dictionary<SortType, string>
+        {
+            { SortType.Ascending, "ASC" },
+            { SortType.Descending, "DESC" }
+        };
+
+        public MySQLParser(MySqlConnection connection, SqlTable sqlTable)
+        {
+            this.connection = connection;
+            this.sqlTable = sqlTable;
+        }
+
+        public void Select(IEnumerable<SqlColumn> columns) { this.selectColumns = columns; }
+
+        public void SortBy(IEnumerable<SqlColumn> columns, SortType sortType)
+        {
+            this.sortColumns = columns;
+            this.sortType = sortType;
+        }
+
+        public async Task<DataTable> GetTable()
+        {
+            string commandString = BuildCommandString();
+            MySqlCommand command = new MySqlCommand(commandString, connection);
+
+            MySqlDataReader dr = await command.ExecuteReaderAsync(CommandBehavior.Default);
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+
+            return dt;
+        }
+
+        private string BuildCommandString()
+        {
+            string commandString = "SELECT ";
+            if (selectColumns == null || selectColumns.Count() == 0) { commandString += '*'; }
+            else
+            {
+                var enumerator = selectColumns.GetEnumerator();
+                enumerator.MoveNext();
+                commandString += columnStrings[enumerator.Current];
+                while (enumerator.MoveNext()) { commandString += $", {columnStrings[enumerator.Current]}"; }
+            }
+
+            commandString += $" FROM {tableStrings[sqlTable]} ";
+            if (sortColumns != null && sortColumns.Count() > 0)
+            {
+                commandString += "ORDER BY ";
+                var enumerator = sortColumns.GetEnumerator();
+                enumerator.MoveNext();
+                commandString += columnStrings[enumerator.Current];
+                while (enumerator.MoveNext()) { commandString += $", {columnStrings[enumerator.Current]}"; }
+            }
+            commandString += ';';
+
+            return commandString;
+        }
+    }
+
+    public enum SqlTable { AdminInfo, UserInfo }
+
+    public enum SqlColumn { ID, Username }
+
+    public enum SortType { None, Ascending, Descending }
+}
